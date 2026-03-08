ee27ad52b94252a3e592dd32f789dd07e896f9f6
Prince Alwyne <princealwyne7@gmail.com>
2026-03-08 03:37:46 +0300

    added secure login authentication system

---
+++ routes/protected.js
@@ -0,0 +1,15 @@
+const express=require("express")
+const router=express.Router()
+
+const auth=require("../middleware/authMiddleware")
+
+router.get("/dashboard",auth,(req,res)=>{
+
+res.json({
+message:"Welcome to secure dashboard",
+user:req.user
+})
+
+})
+
+module.exports=router
